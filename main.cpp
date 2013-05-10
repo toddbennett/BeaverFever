7ba10f7d10067191eb9b2016fd5d04242ff26965
Todd <xtoddxiferx@hotmail.com>
2013-05-09 19:32:20 -0600

    Dug around a little, fixed the exiting bug.

--- main.cpp
+++ main.cpp
@@ -10,10 +10,6 @@
 #include <Windows.h>
 #include <D3D9.h>
 
-const char className[] = "beaverfeverwinclass";
-
-WNDCLASSEX winclass;
-
 /**
  * d3d_init
  *
@@ -25,13 +21,19 @@ int d3d_init(
 	IDirect3DDevice9 **d3dDevice	// OUT
 	)
 {
-	HRESULT result = 0;
-	D3DPRESENT_PARAMETERS params = {};
+	HRESULT result;
+	D3DPRESENT_PARAMETERS params;
+
+	// Supposedly this is the best practice for initializing things. ={}
+	// doesn't work with bitfields or some such thing. I read too much
+	// StackExchange.
+	ZeroMemory(&params, sizeof(params));
 
 	*d3d = Direct3DCreate9(D3D_SDK_VERSION);
 	if (*d3d == NULL) {
-		return 0;
+		return 1;
 	}
+
 	params.Windowed = TRUE;
 	params.SwapEffect = D3DSWAPEFFECT_DISCARD;
 	params.hDeviceWindow = window;
@@ -50,63 +52,45 @@ int d3d_init(
 	return 0;
 }
 
+/**
+ * getLastErrorString
+ *
+ * Returns a string from Windows indicating what the deal is.
+ */
 char *getLastErrorString()
 {
 	DWORD errorCode = GetLastError();
 	LPTSTR errorString;
 
-	FormatMessage(
+	FormatMessageA(
 		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
 		NULL,
 		errorCode,
-		0,
+		NULL,
 		(LPTSTR) &errorString, // not a bug
-		0,
+		NULL,
 		NULL
 	);
 
 	return errorString;
 }
 
-LRESULT CALLBACK WndProc(
-	HWND hwnd,		// handle to window
+LRESULT CALLBACK wndProc(
+	HWND hWnd,		// handle to window
 	UINT uMsg,		// message information
 	WPARAM wParam,	// parameters to message
 	LPARAM lParam	// more parameters to message
 )
 {
-    switch(uMsg)
-    {
-        case WM_CLOSE:
-            DestroyWindow(hwnd);
-        break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-        break;
-        default:
-            return DefWindowProc(hwnd, uMsg, wParam, lParam);
-    }
-    return 0;
-}
-
-/**
- * Fills in our WNDCLASSEX.
- */
-void init_winclass(HINSTANCE hInstance)
-{
-	winclass.cbSize = sizeof(WNDCLASSEX);	// This is stupid.
-	winclass.style = 0; // There's lots of things to do with this, but we don't
-						// need any of them just yet...
-	winclass.lpfnWndProc = WndProc;	// Procedure to handle window
-	winclass.cbClsExtra = 0;
-	winclass.cbWndExtra = 0;
-	winclass.hInstance = hInstance;
-	winclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	winclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	winclass.hbrBackground = (HBRUSH) (COLOR_WINDOW+1);
-	winclass.lpszMenuName = NULL;
-	winclass.lpszClassName = className;
-	winclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	switch (uMsg) {
+	case WM_QUIT:
+		return DestroyWindow(hWnd);
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
 }
 
 /**
@@ -132,53 +116,51 @@ int d3d_draw_test(IDirect3DDevice9 *d3dDevice)
 }
 
 /**
- * This is our entry point because LOL windoze. I hope you're happy!
+ * WinMain
+ *
+ * Entry point, set things up and enter the message loop.
  */
 int WINAPI WinMain(
-	HINSTANCE	hInstance,		// handle to instance of application
+	HINSTANCE	hInstance,		// Instance of application
 	HINSTANCE	hPrevInstance,	// DEPRECATED
-	LPSTR		lpCmdLine,		// cmd args
-	int			nShowCmd		// Google winmain to learn all about this on
-								// MSDN. we should deal with it eventually.
-								// It's a bunch of display hints.
-)
+	LPSTR		lpCmdLine,		// Command line arguments
+	int			nCmdShow		// Display hints
+	)
 {
-	ATOM winclassatom;
+	WNDCLASS wndClass;
 	HWND window;
 	MSG message;
+	BOOL test;
 
 	IDirect3D9 *d3d;
 	IDirect3DDevice9 *d3dDevice;
 
-	// Set up our window class WHY IS THIS SO MUCH WORK
-	// I DON'T EVEN KNOW WHAT A WINDOW CLASS IS
-	init_winclass(hInstance);
-	winclassatom = RegisterClassEx(&winclass);
-	if (!winclassatom) {
-		printf(getLastErrorString());
+	ZeroMemory(&wndClass, sizeof(wndClass));
+	wndClass.lpfnWndProc = wndProc;
+	wndClass.hInstance = hInstance;
+	wndClass.lpszClassName = "Beaver Fever";
+	if(!RegisterClass(&wndClass)) {
+		MessageBox(NULL, getLastErrorString(), "RegisterClass Error", NULL);
 		return 1;
 	}
 	
-	window = CreateWindowEx(
-		WS_EX_CLIENTEDGE,
-		className,
-		"BeaverFever!",
+	window = CreateWindow(
+		"Beaver Fever",
+		"Beaver Fever",
 		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		800,
-		600,
+		0, 0,		// X, Y,
+		800, 600,	// W, H,
 		NULL,
 		NULL,
 		hInstance,
 		NULL
 	);
 	if (!window) {
-		MessageBox(NULL, "CreateWindowEx failed!", NULL, NULL);
+		MessageBox(NULL, getLastErrorString(), "CreateWindow Error", NULL);
 		return 1;
 	}
 	
-	ShowWindow(window, nShowCmd);
+	ShowWindow(window, nCmdShow);
     UpdateWindow(window);
 
 	if (d3d_init(&d3d, window, &d3dDevice)) {
@@ -186,16 +168,13 @@ int WINAPI WinMain(
 		return 1;
 	}
 
-    while (1) {
-		BOOL test;	// Why the fuck did Micro$haft make this a BOOL when
-					// it can be a bunch of different values what the hell
-		d3d_draw_test(d3dDevice);
-		test = GetMessage(&message, window, 0, 0);
-		if (test == 0) {
-			break;
+    while ((test = GetMessage(&message, NULL, 0, 0)) != 0) {
+		if (d3d_draw_test(d3dDevice)) {
+			MessageBox(NULL, "d3d_draw_test failed!", NULL, NULL);
+			return 1;
 		}
 		if (test == -1) {
-			char *s = getLastErrorString();
+			MessageBox(NULL, getLastErrorString(), "GetMessage Error", NULL);
 			return 1;
 		}
 		else {
