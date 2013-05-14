eac6371ba7e57d322c6328f749f7fabfd86ebb6a
Tyson Jeffrey Geib <Superbob345_jr@msn.com>
2013-05-14 12:11:43 -0600

    Keyboarding
    Also a Baxter struct

--- main.cpp
+++ main.cpp
@@ -9,7 +9,34 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <D3D9.h>
+#include <math.h>
 
+#define VK_A 0x0041
+#define VK_D 0x0044
+#define VK_S 0x0053
+#define VK_W 0x0057
+
+
+// Baxter structure. this should be put in a header or something depending on how you want to do it
+struct Baxter{
+float x,y,z;  //coordinates in 3d space
+float facing; //angle of facing in radians. could also do degrees if you prefer. 0 is right along the y-axis
+void init() //funtion that initializes all of Baxter's variables to 0 just in case
+{
+	x=0;
+	y=0;
+	z=0;
+	facing=0;
+}
+void move(float distance)//function that moves Baxter with facing. distance should be passed in as a speed variable. Negative value denotes moving backwards
+{
+	x = x + distance*sin(facing);
+	y = y + distance*cos(facing);
+}
+//obviously more variables later
+};
+
+Baxter *Bax; //Create pointer to instance of Baxter named Bax
 /**
  * d3d_init
  *
@@ -51,7 +78,6 @@ int d3d_init(
 
 	return 0;
 }
-
 /**
  * getLastErrorString
  *
@@ -87,6 +113,19 @@ LRESULT CALLBACK wndProc(
 		return DestroyWindow(hWnd);
 	case WM_DESTROY:
 		PostQuitMessage(0);
+		return 0;
+	case WM_KEYDOWN:
+		switch (wParam)
+	{
+		case VK_A:
+			Bax->facing -= 0.1;
+		case VK_D:
+			Bax->facing += 0.1;
+		case VK_W:
+			Bax->move(0.5);
+		case VK_S:
+			Bax->move(-0.3);
+	}
 		return 0;
 	default:
 		return DefWindowProc(hWnd, uMsg, wParam, lParam);
@@ -135,7 +174,9 @@ int WINAPI WinMain(
 	IDirect3D9 *d3d;
 	IDirect3DDevice9 *d3dDevice;
 	IDirect3DVertexBuffer9 *d3dVertexBuffer;
+	IDirect3DVertexShader9 *pShader;
 
+	Bax->init();
 	ZeroMemory(&wndClass, sizeof(wndClass));
 	wndClass.lpfnWndProc = wndProc;
 	wndClass.hInstance = hInstance;
@@ -181,6 +222,7 @@ int WINAPI WinMain(
 		MessageBox(NULL, getLastErrorString(), "CreateVertexBuffer Error", NULL);
 		return 1;
 	}
+
 
     while ((test = GetMessage(&message, NULL, 0, 0)) != 0) {
 		if (d3d_draw_test(d3dDevice)) {
